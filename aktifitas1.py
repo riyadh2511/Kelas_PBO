6c385b6ee6ff6ec3279ff18460cd05e0a873ddc4
riyadh2511 <riyadhfaridz25@gmail.com>
2021-02-26 09:36:45 +0700

    Riyadh Ahmad Faridz (1909076)

---
+++ aktifitas1.py
@@ -0,0 +1,8 @@
+nama=input("masukkan nama Anda: ")
+umur=20
+
+print("")
+print("====INPUT DAN OUTPUT PADA BAHASA PEMROGRAMAN PHYTON====")
+print("nama Anda:  ", Riyadh Ahmad Faridz)
+print("nama Anda:  ", 20)
+print("")
